2df5190967121d2ddd8448d9e8dbbcd27f36bf42
Noa.O <noa.obringer@gmail.com>
2025-10-13 14:17:34 +0200

    feat: resume link and button to CV section

--- src/components/Cv.tsx
+++ src/components/Cv.tsx
@@ -1,7 +1,35 @@
+import { Button } from './ui/button'
+import { ExternalLinkIcon } from './ui/icons/index'
+import { useRef } from 'react'
+import type { ExternalLinkIconHandle } from './ui/icons/index'
+
 export function Cv() {
+    const externalLinkIconRef = useRef<ExternalLinkIconHandle>(null)
+
     return (
-        <div id="projects" className='flex flex-col gap-4 w-full h-100x'>
+        <div id="cv" className='flex flex-col gap-4 w-full mb-10'>
             <h2 className='text-white/50 text-2xl font-light'>Curriculum Vitae</h2>
+
+            <div className='flex gap-2 w-full justify-between'>
+                <p className='text-white/60 text-lg font-light'>Want to know more about my background and experience?</p>
+
+                <Button
+                    asChild
+                    className="bg-white/80 text-black hover:bg-gray-700 w-fit"
+                    onMouseEnter={() => externalLinkIconRef.current?.startAnimation()}
+                    onMouseLeave={() => externalLinkIconRef.current?.stopAnimation()}
+                >
+                    <a
+                        href="/cv.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-2 hover:scale-105 transition-all duration-300"
+                    >
+                        <ExternalLinkIcon ref={externalLinkIconRef} size={16} />
+                        View Resume
+                    </a>
+                </Button>
+            </div>
         </div>
     )
 }
