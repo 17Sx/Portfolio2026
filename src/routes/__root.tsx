87396ca7971de7b8ec732314973230e12da2061e
Noa.O <noa.obringer@gmail.com>
2025-10-14 16:59:14 +0200

    chore: remove Route export from __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -96,23 +96,3 @@ function NotFound() {
     </div>
   )
 }
-
-export const Route = createRootRoute({
-  component: () => (
-    <div className="flex flex-col h-screen w-full bg-[#111111]">
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </div>
-  ),
-  notFoundComponent: NotFound,
-})
